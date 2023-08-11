ecedae7d571ef7217e59b8964775d09a50196da7
devrahulsingh <rahulsinghrajput6090@gmail.com>
2023-08-11 15:20:59 +0530

    initial

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,15 @@
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 // import '../src/.env'
 
-export const initSocket = async () =>{
+export const initSocket = async() => {
 
     const options = {
-        'force new connection' : true, 
-        reconnectionAttempt : 'Infinity',
-        timeout : 10000,
-        transports : ['websocket']
-    }
-    // return io(process.env.REACT_APP_SERVER_URL, options)
-    return io("http://localhost:5000", options)
+            'force new connection': true,
+            reconnectionAttempt: 'Infinity',
+            timeout: 10000,
+            transports: ['websocket']
+        }
+        // return io(process.env.REACT_APP_SERVER_URL, options)
+        // return io("http://4.188.239.121:5001/", options)
+    return io("http://localhost:5000/", options)
 }
